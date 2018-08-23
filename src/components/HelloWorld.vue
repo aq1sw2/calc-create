5da01cd18d3af92cd17ef3e579c5066d5baed56b
yul <29713862@qq.com>
2018-08-23 16:39:18 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-row>
+        <el-radio v-model="radio" label="1">备选项</el-radio>
+        <el-radio v-model="radio" label="2">备选项</el-radio>
+        <el-radio v-model="radio" label="3">备选项</el-radio>
+      </el-row>
+      <el-row>
+        <el-radio v-model="radio" label="1">备选项</el-radio>
+        <el-radio v-model="radio" label="2">备选项</el-radio>
+        <el-radio v-model="radio" label="3">备选项</el-radio>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12"><el-button type="primary" plain>主要按钮</el-button></el-col>
+        <el-col :span="12"><el-checkbox v-model="checked">备选项</el-checkbox></el-col>
+      </el-row>
+    </el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        radio: '1',
+        checked: true
+      };
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center; 
+  }
+ 
+ 
+</style>
