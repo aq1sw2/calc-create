58e72b71915c4465574c132d41943903ddd95127
yul <29713862@qq.com>
2018-08-23 17:56:39 +0800

    update ui

---
+++ src/components/Qusetion.vue
@@ -0,0 +1,98 @@
+<template>
+   <el-container>
+      <el-header>
+        <el-row>     
+          <el-button type="primary">主要按钮</el-button>
+          <el-button type="success">成功按钮</el-button>
+        </el-row>
+      </el-header>
+      <el-main> 
+        <el-row>
+          <el-radio-group v-model="radio3">
+            <el-radio-button label="上海"></el-radio-button>
+            <el-radio-button label="北京"></el-radio-button>
+            <el-radio-button label="广州"></el-radio-button>
+            <el-radio-button label="深圳"></el-radio-button>
+          </el-radio-group>
+        </el-row>
+        <hr>
+        <el-row>
+          <el-radio-group v-model="radio3">
+            <el-radio-button label="上海"></el-radio-button>
+            <el-radio-button label="北京"></el-radio-button>
+            <el-radio-button label="广州"></el-radio-button>
+            <el-radio-button label="深圳"></el-radio-button>
+          </el-radio-group>
+        </el-row>
+        <el-row>
+        <el-table
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="日期"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="地址">
+          </el-table-column>
+        </el-table>
+        </el-row>
+      </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      radio: "1",
+      checked: true,
+      tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+  body > .el-container {
+    margin-bottom: 20px;
+  }
+</style>
